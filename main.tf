0b0cd0400849513284bc59abf7b7b4b14ba41259
lferdinand <lferdinand@microsoft.com>
2025-07-18 05:12:06 -0700

    fix: revert uncommented backend

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = "~> 1.9"
-  # backend "azurerm" {
-  # use_azuread_auth     = true
-  # }
+  backend "azurerm" {
+  use_azuread_auth     = true
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
