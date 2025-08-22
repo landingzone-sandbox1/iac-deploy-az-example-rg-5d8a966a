b1d54d26c114e0028af47989d537862f504dab30
Lorrin Ferdinand <lferdinand@microsoft.com>
2025-08-22 16:19:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "azure_rg_example" {
   # source = "./module-source/iac-mod-az-resource-group"
   # For production, use:
   # tflint-ignore: terraform_module_pinned_source
-  source = "git::ssh://git@github.com/landingzone-sandbox/iac-mod-az-resource-group.git?ref=rsg-mod-20-08-25"
+  source = "git::ssh://git@github.com/landingzone-sandbox/iac-mod-az-resource-group.git"
 
   # Pass-through variables directly to the child module
   location              = var.location
